32badb39e969b90f9b10ed123acaf831a18777d1
Austin Quenneville <austin.quenneville@whitecase.com>
2025-03-16 17:28:03 -0400

    update scripts

--- scripts/install-k3s.sh
+++ scripts/install-k3s.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-curl -sfL https://get.k3s.io | INSTALL_K3S_EXEC="server --flannel-backend=wireguard" sh -
+curl -sfL https://get.k3s.io | INSTALL_K3S_EXEC="server --flannel-backend=wireguard-native" sh -
--- scripts/join-node.sh
+++ scripts/join-node.sh
@@ -1,26 +1,15 @@
 #!/bin/bash
 
-# Define control plane addresses
-PRIVATE_IP="192.168.1.228"  # Change this to your private control plane IP
-PUBLIC_IP="65.34.87.212"  # Change this to your public control plane IP
+CONTROL_PLANE_IP="$1"
 
 # Check input argument
 if [ "$#" -ne 2 ]; then
-    echo "Usage: $0 <local|remote> <k3s_node_token>"
+    echo "Usage: $0 <control_plane_ip> <k3s_node_token>"
     exit 1
 fi
 
-MODE=$1
 K3S_TOKEN=$2
 
-if [ "$MODE" == "local" ]; then
-    K3S_URL="https://${PRIVATE_IP}:6443"
-elif [ "$MODE" == "remote" ]; then
-    K3S_URL="https://${PUBLIC_IP}:6443"
-else
-    echo "Invalid argument. Use 'local' for private IP or 'remote' for public IP."
-    exit 1
-fi
 
 # Install K3s agent
 echo "Joining K3s cluster as ${MODE} node..."
